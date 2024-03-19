b3b4832ceda5fe8ca35fa9fc7e59183ec8aae9fd
deepakg-sacumen <deepak.g@sacumen.com>
2024-03-19 12:36:22 +0530

    Made changes

--- add.py
+++ add.py
@@ -1,5 +1,17 @@
-def add(a, b):
-    return a + b
+"""Program to add two nums."""
+
+
+def add(num1: int, num2: int) -> int:
+    """Sum of two numbers.
+
+    Args :
+        num1 (int) : int input
+        num2 (int) : int input
+
+    Returns :
+        int : sum of two num.
+    """
+    return num1 + num2
 
 
 print(add(1, 2))
